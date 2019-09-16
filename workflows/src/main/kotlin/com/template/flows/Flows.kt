7f601d9acb734363fca96f2b3509ade587bd9ca8
Phoenix Zerin <phoenix@blockchainlabs.nz>
2019-09-16 14:40:15 +1200

    Implemented "Hello, world!" CorDapp.
    https://docs.corda.net/hello-world-introduction.html

--- workflows/src/main/kotlin/com/template/flows/Flows.kt
+++ workflows/src/main/kotlin/com/template/flows/Flows.kt
@@ -1,27 +1,43 @@
 package com.template.flows
 
 import co.paralleluniverse.fibers.Suspendable
+import com.template.contracts.TemplateContract
+import com.template.states.IOUState
+import net.corda.core.contracts.Command
 import net.corda.core.flows.*
+import net.corda.core.identity.Party
+import net.corda.core.transactions.TransactionBuilder
 import net.corda.core.utilities.ProgressTracker
 
-// *********
-// * Flows *
-// *********
 @InitiatingFlow
 @StartableByRPC
-class Initiator : FlowLogic<Unit>() {
+class IOUFlow(val iouValue: Int,
+              val otherParty: Party) : FlowLogic<Unit>() {
     override val progressTracker = ProgressTracker()
 
     @Suspendable
     override fun call() {
-        // Initiator flow logic goes here.
+        val notary = serviceHub.networkMapCache.notaryIdentities[0]
+
+        val outputState = IOUState(iouValue, ourIdentity, otherParty)
+        val command = Command(TemplateContract.Commands.Action(), ourIdentity.owningKey)
+
+        val txBuilder = TransactionBuilder(notary = notary)
+                .addOutputState(outputState, TemplateContract.ID)
+                .addCommand(command)
+
+        val signedTx = serviceHub.signInitialTransaction(txBuilder)
+
+        val otherPartySessions = initiateFlow(otherParty)
+
+        subFlow(FinalityFlow(signedTx, otherPartySessions))
     }
 }
 
-@InitiatedBy(Initiator::class)
-class Responder(val counterpartySession: FlowSession) : FlowLogic<Unit>() {
+@InitiatedBy(IOUFlow::class)
+class IOUResponderFlow(private val otherPartySession: FlowSession) : FlowLogic<Unit>() {
     @Suspendable
     override fun call() {
-        // Responder flow logic goes here.
+        subFlow(ReceiveFinalityFlow(otherPartySession))
     }
 }
